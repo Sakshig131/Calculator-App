406856b797957653fb270b23d90c92effb6686cb
Sakshi <sakshigaikwad6788@gmail.com>
2026-03-27 22:43:49 +0530

    CALCULATOR-APP

---
+++ script.js
@@ -0,0 +1,22 @@
+function appendValue(value) {
+    let display = document.getElementById("display");
+    display.value += value;
+}
+
+function clearDisplay() {
+    document.getElementById("display").value = "";
+}
+
+function calculate() {
+    let display = document.getElementById("display");
+
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
+}
+function backspace() {
+    let display = document.getElementById("display");
+    display.value = display.value.slice(0, -1);
+}
